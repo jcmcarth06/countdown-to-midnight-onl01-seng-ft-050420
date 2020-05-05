326ce30a38b1322c49a62975d74f76c3268ff9ae
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-05 22:13:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,11 @@
 #write your code here
 
 def countdown
-  if x == 0
-    puts "HAPPY NEW YEAR!"
-  end
     x = 10
   while x <= 10
-    puts "#{x}"
-    x -= 1
+    if x == 0
+      puts "HAPPY NEW YEAR!"
+    else puts "#{x}"
+      x -= 1
   end
 end
